d027816716d34b1d4991a973ecf01434e1067810
Dylan Magne <valesmoss@hotmail.fr>
2022-10-08 10:56:43 +0200

    Version finale

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -25,7 +25,8 @@ exports.login = (req, res ,next) => {
         bcrypt.compare(req.body.password, user.password)
         .then(valid => {
             if (!valid) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' })
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' }),
+                console.log('toto')
             }
             res.status(200).json({ 
                 userId: user._id,
