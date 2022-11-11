3aaf5af564757c4adba0ac0e03c2c2207315264d
Dylan Magne <valesmoss@hotmail.fr>
2022-11-11 09:17:05 +0100

    nouveaux changements

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -41,12 +41,9 @@ exports.updateSauce = (req, res, next) => { /* permet de modifier une sauce grâ
            if (sauce.userId != req.auth.userId) {
                res.status(403).json({ message : 'Not authorized'});
            } else {
-               const filename = sauce.imageUrl.split("/images/")[1]
-               fs.unlink(`images/${filename}`, () => {
                Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
                .then(() => res.status(200).json({message : 'Objet modifié!'}))
                .catch(error => res.status(401).json({ error }));
-              })
            }
        })
        .catch((error) => {
@@ -59,13 +56,14 @@ exports.deleteSauce = (req, res, next) => { /* permet de supprimer une sauce en 
   .then(sauce => {
     if (sauce.userId != req.auth.userId) {
       res.status(403).json({ message : 'Not authorized'});
-  }
+    } else {
     const filename = sauce.imageUrl.split("/images/")[1]
     fs.unlink(`images/${filename}`, () => {
     Sauce.deleteOne({_id : req.params.id})
   .then(res.status(200).json({ message: "Sauce supprimée" }))
   .catch(error => res.status(400).json({ error }))
     })
+    }
   })
   .catch(error => res.status(500).json({ error }))
 }
@@ -83,8 +81,12 @@ exports.likeDislikeSauce = (req, res, next) => {
         Sauce.updateOne({ _id: sauceId }, { $push: { usersLiked: userId }, $inc: { likes: +1 }})
         .then(() => res.status(200).json({ message: `J'aime` }))
         .catch((error) => res.status(400).json({ error }))
+        console.log('LIKE +1')
+      } else {
+        res.status(400).json({ message: 'Vous ne pouvez plus' })
       }
            })
+           .catch((error) => res.status(404).json({ error }))
       break
 
     case 0 : /* permet d'être ou de retourner a une situation neutre ou l'utilisateur ne like ou dislike pas le sauce */
@@ -95,7 +97,7 @@ exports.likeDislikeSauce = (req, res, next) => {
                 .then(() => res.status(200).json({ message: `Neutre` }))
                 .catch((error) => res.status(400).json({ error }))
             }
-            if (sauce.usersDisliked.includes(userId)) { 
+           else if (sauce.usersDisliked.includes(userId)) { 
               Sauce.updateOne({ _id: sauceId }, { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 }})
                 .then(() => res.status(200).json({ message: `Neutre` }))
                 .catch((error) => res.status(400).json({ error }))
@@ -113,9 +115,10 @@ exports.likeDislikeSauce = (req, res, next) => {
           .catch((error) => res.status(400).json({ error }))
             }
            })
+           .catch((error) => res.status(404).json({ error }))
       break
       
       default:
-        console.log(error)
+        res.status(400).json({ message: 'toto' })
   }
 }
